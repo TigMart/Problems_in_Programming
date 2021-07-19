ffe432b017d2f8ff916d6a6a5551a2c47894d2cf
Tigran <77511200+TigMart@users.noreply.github.com>
2021-07-20 00:47:25 +0400

    Problem - 18

---
+++ One_dimensional_arrays_Middle_level/Problem_18/Problem_18.cpp
@@ -0,0 +1,42 @@
+// Problem_18.cpp : 
+
+#include <iostream>
+#include <ctime>
+
+template<int n>
+void foo(int(&arr)[n]);
+
+int main()
+{
+    srand((int)time(0));
+    const int size = 10;
+    int arr[size]{ 0 };
+    //cin
+    for (int i = 0; i < size; ++i)
+    {
+        arr[i] = rand() % 30;
+    }
+    //cout
+    std::cout << std::endl << "Array: ";
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    foo(arr);
+    std::cout << std::endl;
+    return 0;
+}
+
+template<int n>
+void foo(int(&arr)[n])
+{
+    int count = 0;
+    for (int i = 1; i < n - 1; ++i)
+    {
+        if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1])
+            ++count;
+    }
+    std::cout << "Count: " << count << std::endl;
+}
+
